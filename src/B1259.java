dd3226bda2212274843fde703bf1101a4dfb3eb1
strawberrysunday <132540289+strawberrysunday@users.noreply.github.com>
2024-03-21 05:58:38 +0900

    240321 B11259 StringBuilder Initialization Mistake

--- src/B1259.java
+++ src/B1259.java
@@ -6,12 +6,8 @@ public class B1259 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        /*
-         * 글자길이가 1 or 2 일 때 처리해야함
-         * 엥
-         * 그냥 다 틀렸네 ㅠㅠ 이거우뜨캐해ㅐㅐㅐㅐㅐㅐ
-         *
-         */
+
+
         while (true) {
             String str = br.readLine();
 
@@ -19,36 +15,33 @@ public class B1259 {
                 break;
             }
 
-            char [] tmpArr = str.toCharArray();
-            int isPalindrome = 0;
-            //System.out.println("str.length() = " + str.length());
-            int index = str.length() / 2;
-            //System.out.println("before for loop index = " + index);
+            sb = new StringBuilder(str);
+            //    /*이 시점에서 StringBuilder가 초기화되기 때문에 앞의 결과가 사라진다. */
 
 
 
-            for (int i = 0; i < index; i++) {
-
-                if (tmpArr[index+i] == tmpArr[index-i]) {
-                    System.out.println("i = " + i);
-                    System.out.println("(index-i) = " + (index-i));
-                    System.out.println("(index+i) = " + (index+i));
-                    isPalindrome++;
-
-                    //System.out.println("isPalindrome = " + isPalindrome);
-                }
-            }
+            String reverseStr = sb.reverse().toString();
 
 
-            if(isPalindrome == index){
+            sb.delete(0, sb.length());
+
+
+
+
+
+            if (str.equals(reverseStr)) {
+
                 sb.append("yes");
+
             }else {
+
                 sb.append("no");
             }
 
+
             sb.append("\n");
 
-            //System.out.println("------------");
+
         }
 
         System.out.println(sb);
