2ce04c11b13bf6d54a3990cc501d70a10e3ab1c9
strawberrysunday <132540289+strawberrysunday@users.noreply.github.com>
2024-04-01 15:58:46 +0900

    240401 B1463 commit

--- src/B2667.java
+++ src/B2667.java
@@ -2,18 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 
-public class B2667{
+public class B2667 {
     static int N, count;
-    static int [][] apart ;
-    static boolean [][] visited;
+    static int[][] apart;
+    static boolean[][] visited;
     static StringBuilder sb = new StringBuilder();
     static LinkedList<Integer> countApart;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-
-
 
 
     public static void main(String[] args) throws IOException {
@@ -25,22 +24,23 @@ public class B2667{
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < N; j++) {
-                if(str.charAt(j)=='1'){
+                if (str.charAt(j) == '1') {
                     apart[i][j] = 1;
                 }
             }
         }
 
-        count = 0 ;
+        count = 1;
         visited = new boolean[N][N];
         countApart = new LinkedList<Integer>();
 
 
         for (int i = 0; i < N; i++) {
-            for(int j = 0; j< N; j++){
+            for (int j = 0; j < N; j++) {
                 if (apart[i][j] == 1 && !visited[i][j]) {
-                    count =1;
                     dfs(i, j);
+                    countApart.add(count);
+                    count = 1;
                 }
             }
         }
@@ -48,9 +48,13 @@ public class B2667{
         System.out.println(countApart.size());
 
 
-        Arrays.sort(new LinkedList[]{countApart});
+        Collections.sort(countApart);
 
-        System.out.println(countApart);
+        for (int i = 0; i < countApart.size(); i++) {
+
+            System.out.println(countApart.get(i));
+
+        }
     }
 
     static void dfs(int x, int y) {
@@ -64,36 +68,20 @@ public class B2667{
             int tmpX = x + dx[i];
             int tmpY = y + dy[i];
 
-            if(0>tmpX||tmpX>=N||0>tmpY||tmpY>=N){
+            if (0 > tmpX || tmpX >= N || 0 > tmpY || tmpY >= N) {
                 continue;
             }
-            if (apart[tmpX][tmpY]==0|| visited[tmpX][tmpY]){
+            if (apart[tmpX][tmpY] == 0 || visited[tmpX][tmpY]) {
                 continue;
             }
 
-            if(apart[x][y]!=0 && !visited[tmpX][tmpY] ){
+            if (apart[x][y] != 0 && !visited[tmpX][tmpY]) {
                 count++;
-                visited[tmpX][tmpY]=true;
+                visited[tmpX][tmpY] = true;
                 dfs(tmpX, tmpY);
-                countApart.add(count);
 
             }
         }
 
     }
-
-
-    static class Location{
-        int x;
-        int y;
-
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
 }
-
-
-
