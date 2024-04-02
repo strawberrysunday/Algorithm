357d704023d25ec453cd3f5837245199831631ac
strawberrysunday <132540289+strawberrysunday@users.noreply.github.com>
2024-04-02 15:32:21 +0900

    240402 B5585 commit

--- src/B5585.java
+++ src/B5585.java
@@ -9,38 +9,42 @@ public class B5585 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N =1000- Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        //디버깅 1입력시 count -554115029 나옴 뭥미 -ㅅ-??
+        N =1000-N ;
+
         int count =0;
 
-        while (N > 0) {
+        while (N >= 0) {
 
-            if(N >=500){
-
-                N = N - 500;
-                count++;
-            }else if(N>=100){
-                N = N - 100;
-                count++;
-            }else if(N>=50) {
-                N = N - 50;
-                count++;
-            }else if(N>=10) {
-                N = N - 10;
-                count++;
-            }else if(N>=5){
-                N = N - 5;
-                count++;
-            }else{
-                count += N;
+            if (N == 0) {
+            break;
             }
- }
+                if (N >= 500) {
 
+                    N = N - 500;
+                    count += 1;
+                } else if (N >= 100) {
+                    N = N - 100;
+                    count += 1;
+                } else if (N >= 50) {
+                    N = N - 50;
+                    count += 1;
+                } else if (N >= 10) {
+                    N = N - 10;
+                    count += 1;
+                } else if (N >= 5) {
+                    N = N - 5;
+                    count += 1;
+                } else {
+                    count += N;
+                    N = 0;
+                }
+            }
         System.out.println(count);
+    }
+
 
 
     }
 
-
-}
