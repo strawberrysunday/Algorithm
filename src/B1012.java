59a6319770fb6b3f821460a8254235903c3ee690
strawberrysunday <132540289+strawberrysunday@users.noreply.github.com>
2024-04-03 15:42:01 +0900

    240402 B1012 commit

--- src/B1012.java
+++ src/B1012.java
@@ -12,64 +12,66 @@ public class B1012 {
     static int[] dy = {0, 0, 1, -1};
     static boolean[][] visited;
     static int[][] Map;
-    static LinkedList<Integer> bugs = new LinkedList<>();
+    //static LinkedList<Integer> bugs;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        T = Integer.parseInt(br.readLine());
-        while (T-- > 0) {
 
-            String str = br.readLine();
-            st = new StringTokenizer(str);
+        T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            bug = 0;
+            int a = 0;
+            StringTokenizer  st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
-            bug = 0;
+            Map = new int[M + 1][N + 1];
+            visited = new boolean[M + 1][N + 1];
+            //bugs = new LinkedList<>();
 
-            Map = new int[M][N];
-            visited = new boolean[M][N];
 
-            while (K-- > 0) {
+            for (int i = 0; i < K; i++) {
 
-                str = br.readLine();
-                st = new StringTokenizer(str);
-
+                st = new StringTokenizer(br.readLine()," ");
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-
                 Map[x][y] = 1;
-
             }
 
 
-            bug = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
+            for (int i = 0; i < M; i++) {
+                //bug = 0;
+                for (int j = 0; j < N; j++) {
 
+                    if (Map[i][j] == 1 && !visited[i][j]) {
 
-                    if (Map[i][j] == 1 || !visited[i][j]) {
-                        bug=1;
                         DFS(i, j);
+                        bug = bug + 1;
+                        a++;
 
                     }
 
                 }
 
-                bugs.add(bug);
-                bug= 0;
+
             }
 
-            System.out.println(bugs.size());
+            // 소름 bug= bug+1은 왜 답이 이상하게 나올까
+            System.out.println( bug);
+           // System.out.println( a);
 
         }
 
 
     }
 
+
     static void DFS(int x, int y) {
+
+
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
@@ -77,27 +79,15 @@ public class B1012 {
             int newX = x + dx[i];
             int newY = y + dy[i];
 
-            if (newX < 0 || M < newX || newY < 0 || N < newY) continue;
-            if (Map[newX][newY] == 0 || visited[newX][newY]) continue;
+            if (newX < 0 ||  M < newX || newY < 0 || N < newY) continue;
+            if (Map[newX][newY] != 1 || visited[newX][newY]) continue;
 
             DFS(newX, newY);
-            bug = bug + 1;
-            visited[newX][newY] = true;
+
 
         }
 
 
-    }
-
-
-    static class cabbage {
-        int x;
-        int y;
-
-        public cabbage(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 
 
