d3830b2f2a7784b3fd76203a9690dac887eb19ce
strawberrysunday <132540289+strawberrysunday@users.noreply.github.com>
2024-04-04 18:06:01 +0900

    240403 B1931 commit new Comparator<int[]>(){} (o1,o2) -> {}

--- src/B1931.java
+++ src/B1931.java
@@ -1,67 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class B1931 {
 
-    public void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        lecture [] lectures = new lecture[N];
-        int[] runningTimes = new int[N];
 
+		/*
+		  time[][0] 은 시작시점을 의미
+		  time[][1] 은 종료시점을 의미
+		*/
+        int[][] time = new int[N][2];
 
-        for (int i = 0; i < lectures.length; i++) {
-            String str = br.readLine();
-            StringTokenizer st = new StringTokenizer(str, " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
 
-            lectures[i] = new lecture(start, end);
-
-        }
-
-        for (int i = 0; i < runningTimes.length; i++) {
-
-            runningTimes[i] = lectures[i].getRunningTime();
-
-
-
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            time[i][0] = Integer.parseInt(st.nextToken());	// 시작시간
+            time[i][1] = Integer.parseInt(st.nextToken());	// 종료시간
         }
 
 
+        // 끝나는 시간을 기준으로 정렬하기 위해 compare 재정의
+        Arrays.sort(time, (o1, o2) -> {
 
+            // 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
 
+            return o1[1] - o2[1];
+        });
 
+        int count = 0;
+        int prev_end_time = 0;
 
+        for(int i = 0; i < N; i++) {
 
+            // 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신
+            if(prev_end_time <= time[i][0]) {
+                prev_end_time = time[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 
-    class lecture{
-        int start;
-        int end;
-
-        int runningTime;
-
-        public lecture(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        public int getRunningTime() {
-            return end-start;
-        }
-
-        public int getStart() {
-            return start;
-        }
-
-        public int getEnd() {
-            return end;
-        }
-    }
 }
