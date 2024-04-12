4e78131a8de39e8fa994c3368fcd58ae90949559
strawberrysunday <132540289+strawberrysunday@users.noreply.github.com>
2024-04-12 17:15:39 +0900

    240412 B1920 왜 틀렸지?

---
+++ src/B1920.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1920 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        int[] target = new int[M];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            target[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+
+            int tmp = binarySearch(arr, target[i]);
+            if(tmp ==-1){
+
+                sb.append("0").append("\n");
+            }
+            else{
+
+                sb.append("1").append("\n");
+            }
+
+        }
+
+        System.out.println(sb);
+    }
+
+
+    static int binarySearch(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start <= end) {
+
+            int mid =(start+end) / 2;
+
+            if(target<arr[mid]){
+
+                end = mid-1;
+
+            } else if (arr[mid] < target) {
+               start =mid+1;
+            } else if (arr[mid] == target) {
+
+                return mid;
+
+            }
+
+
+
+        }
+        return -1;
+
+
+
+
+    }
+
+}
